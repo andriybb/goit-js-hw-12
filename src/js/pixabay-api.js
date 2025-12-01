e87d19a0b1049034219ac170b675d17c8c866ffa
andriy <and123bar@gmail.com>
2025-12-01 22:44:19 +0200

    f2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-
-
-const API_KEY = import.meta.env.VITE_PIXABAY_KEY;
+const API_KEY = '53379433-198b2b18e5117643ffa461def' ; 
 const baseUrl = 'https://pixabay.com/api/';
 
 export async function getImagesByQuery(query, page) {
